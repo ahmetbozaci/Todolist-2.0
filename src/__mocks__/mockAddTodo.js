5168e36a01b369b0ea8958c35c61b0991a4cde3e
ahmetbozaci <ahmt9417@gmail.com>
2021-07-28 22:08:36 +0300

    Create test for addTodo function

---
+++ src/__mocks__/mockAddTodo.js
@@ -0,0 +1,16 @@
+/**
+ * @jest-environment jsdom
+ */
+
+const addTodo = (tasks) => {
+  const inputText = document.getElementById('input-text');
+  const newItem = {
+    index: Number(`${tasks.length}`),
+    description: `${inputText.value}`,
+    completed: false,
+  };
+  tasks.push(newItem);
+  return tasks;
+};
+
+export default addTodo;
--- src/__test__/addTodo.test.js
+++ src/__test__/addTodo.test.js
@@ -2,16 +2,13 @@
  * @jest-environment jsdom
  */
 
-import addTodoTextToBox from '../addTodo';
-// const addTodoTextToBox = require('../addTodo')
+import addTodo from '../__mocks__/mockAddTodo';
 
-describe('addTodoTextToBox', () => {
-  
-
-  
-  it('something', () => {
-    expect(addTodoTextToBox()).toBe()
+describe('addTodo', () => {
+  document.body.innerHTML = '<input id="input-text" value="Learn webpack">'
+  const inputText = document.getElementById('input-text');
+  const tasks = [];
+  it('Should return task array with attached todo object', () => {
+    expect(addTodo(tasks)).toBe(tasks)
   });
-
-
 })
