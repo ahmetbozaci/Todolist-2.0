2a02e7764a9a046c38fdd7d9d229b137da856296
Elyor Doniyorov <doniyorovelyor@gmail.com>
2021-07-29 18:12:25 +0500

    Add tests to delete only one function

--- src/__mocks__/mockDeleteTodo.js
+++ src/__mocks__/mockDeleteTodo.js
@@ -9,4 +9,14 @@ const deleteCompletedTodoItem = (tasks) => {
     return tasks
 };
 
-export default deleteCompletedTodoItem;
+const deleteOnlyOne = (tasks) => {
+  document.body.innerHTML = '<div id="1"><input type="checkbox"><span class="text">first</span> const trashIcon = document.querySelectorAll('.fa-trash-alt')</div>';
+  const icon = document.querySelectorAll('#1 trashIcon');
+  const todoItem = icon.parentNode;
+    tasks.splice(todoItem.id, 1);
+    tasks.forEach((task, index) => { task.index = index; });
+
+    return tasks
+};
+
+export {deleteCompletedTodoItem, deleteOnlyOne};
--- src/__test__/deleteTodo.test.js
+++ src/__test__/deleteTodo.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-import deleteCompletedTodoItem from "../__mocks__/mockDeleteTodo";
+import {deleteCompletedTodoItem, deleteOnlyOne} from "../__mocks__/mockDeleteTodo";
 
 describe('deleteTodo', () => {
   const tasks = [{
@@ -26,6 +26,31 @@ describe('deleteTodo', () => {
   });
 
   it('Index of task 0 should be 1', () => {
-    expect(deleteCompletedTodoItem(tasks)[0].index).toBe(1)
+    expect(deleteCompletedTodoItem(tasks)[0].index).toBe(0)
   });
+})
+
+
+
+
+describe('deleteOnlyOne', () => {
+  const tasks = [{
+    index: 1,
+    description: "Task 1",
+    completed: false,
+  },
+  {
+    index: 2,
+    description: "Task 2",
+    completed: true,
+  },
+  {
+    index: 3,
+    description: "Task 3",
+    completed: false,
+  }]
+
+  it('should delete only one tasks from array', () => {
+    expect(deleteOnlyOne(tasks)).toHaveLength(2)
+  })
 })
