df10afc16bc60e467bde9fba3be65c81a031460b
ahmetbozaci <ahmt9417@gmail.com>
2021-07-29 10:48:40 +0300

    Update addTodo test file

--- src/__test__/addTodo.test.js
+++ src/__test__/addTodo.test.js
@@ -6,9 +6,14 @@ import addTodo from '../__mocks__/mockAddTodo';
 
 describe('addTodo', () => {
   document.body.innerHTML = '<input id="input-text" value="Learn webpack">'
-  const inputText = document.getElementById('input-text');
   const tasks = [];
+
   it('Should return task array with attached todo object', () => {
-    expect(addTodo(tasks)).toBe(tasks)
+    expect(addTodo(tasks)).toHaveLength(1)
   });
+
+  it('Should return task array with attached todo object', () => {
+    expect(addTodo(tasks)).toHaveLength(1)
+  });
+
 })
