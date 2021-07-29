ed753837e622345ba032c3226e4914d274cb4471
ahmetbozaci <ahmt9417@gmail.com>
2021-07-29 16:48:37 +0300

    Add test for deleteOne function

--- src/__mocks__/mockDeleteTodo.js
+++ src/__mocks__/mockDeleteTodo.js
@@ -1,22 +1,23 @@
+/** @format */
+
 const deleteCompletedTodoItem = (tasks) => {
- 
-    function finished(todo) {
-      return todo.completed === false;
-    }
+  function finished(todo) {
+    return todo.completed === false;
+  }
 
-    tasks = tasks.filter(finished);
-    tasks.forEach((task, index) => { task.index = index; });
-    return tasks
+  tasks = tasks.filter(finished);
+  tasks.forEach((task, index) => {
+    task.index = index;
+  });
+  return tasks;
 };
 
-const deleteOnlyOne = (tasks) => {
-  document.body.innerHTML = '<div id="1"><input type="checkbox"><span class="text">first</span> const trashIcon = document.querySelectorAll('.fa-trash-alt')</div>';
-  const icon = document.querySelectorAll('#1 trashIcon');
-  const todoItem = icon.parentNode;
-    tasks.splice(todoItem.id, 1);
-    tasks.forEach((task, index) => { task.index = index; });
-
-    return tasks
+const deleteOnlyOne = (tasks, index) => {
+  tasks.splice(index, 1);
+  tasks.forEach((task, index) => {
+    task.index = index;
+  });
+  return tasks;
 };
 
-export {deleteCompletedTodoItem, deleteOnlyOne};
+export { deleteCompletedTodoItem, deleteOnlyOne };
--- src/__test__/deleteTodo.test.js
+++ src/__test__/deleteTodo.test.js
@@ -1,56 +1,74 @@
 /**
+ * @format
  * @jest-environment jsdom
  */
 
-import {deleteCompletedTodoItem, deleteOnlyOne} from "../__mocks__/mockDeleteTodo";
+import {
+  deleteCompletedTodoItem,
+  deleteOnlyOne,
+} from '../__mocks__/mockDeleteTodo';
 
 describe('deleteTodo', () => {
-  const tasks = [{
-    index: 1,
-    description: "Task 1",
-    completed: false,
-  },
-  {
-    index: 2,
-    description: "Task 2",
-    completed: true,
-  },
-  {
-    index: 3,
-    description: "Task 3",
-    completed: false,
-  }]
+  const tasks = [
+    {
+      index: 1,
+      description: 'Task 1',
+      completed: false,
+    },
+    {
+      index: 2,
+      description: 'Task 2',
+      completed: true,
+    },
+    {
+      index: 3,
+      description: 'Task 3',
+      completed: false,
+    },
+  ];
 
   it('Should delete tasks which are completed', () => {
-    expect(deleteCompletedTodoItem(tasks)).toHaveLength(2)
+    expect(deleteCompletedTodoItem(tasks)).toHaveLength(2);
   });
 
   it('Index of task 0 should be 1', () => {
-    expect(deleteCompletedTodoItem(tasks)[0].index).toBe(0)
+    expect(deleteCompletedTodoItem(tasks)[0].index).toBe(0);
   });
-})
-
-
-
+});
 
 describe('deleteOnlyOne', () => {
-  const tasks = [{
-    index: 1,
-    description: "Task 1",
-    completed: false,
-  },
-  {
-    index: 2,
-    description: "Task 2",
-    completed: true,
-  },
-  {
-    index: 3,
-    description: "Task 3",
-    completed: false,
-  }]
+ 
+  const tasks = [
+    {
+      index: 0,
+      description: 'Task 1',
+      completed: false,
+    },
+    {
+      index: 1,
+      description: 'Task 2',
+      completed: true,
+    },
+    {
+      index: 2,
+      description: 'Task 3',
+      completed: false,
+    },
+  ];
 
   it('should delete only one tasks from array', () => {
-    expect(deleteOnlyOne(tasks)).toHaveLength(2)
-  })
-})
+    expect(deleteOnlyOne(tasks,2)).toHaveLength(2);
+  });
+
+  it('Index should update after delete one item', () => {
+    expect(tasks[1].index).toBe(1);
+  });
+
+  it('Should return update array', () => {
+    expect(deleteOnlyOne(tasks,0)).toEqual([{
+      index: 0,
+      description: 'Task 2',
+      completed: true,
+    }]);
+  });
+});
