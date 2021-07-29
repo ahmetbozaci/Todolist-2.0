9ae4607d5c9d17b73f511fd4580f937cf1b9a9ba
Elyor Doniyorov <doniyorovelyor@gmail.com>
2021-07-29 14:49:01 +0500

    Add tests for delete clear all

--- src/__mocks__/mockDeleteTodo.js
+++ src/__mocks__/mockDeleteTodo.js
@@ -3,8 +3,10 @@ const deleteCompletedTodoItem = (tasks) => {
     function finished(todo) {
       return todo.completed === false;
     }
+
     tasks = tasks.filter(finished);
     tasks.forEach((task, index) => { task.index = index; });
+    return tasks
 };
 
 export default deleteCompletedTodoItem;
--- src/__test__/deleteTodo.test.js
+++ src/__test__/deleteTodo.test.js
@@ -1,5 +1,31 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import deleteCompletedTodoItem from "../__mocks__/mockDeleteTodo";
 
 describe('deleteTodo', () => {
-  
+  const task = [{
+    index: 1,
+    description: "Task 1",
+    completed: false,
+  },
+  {
+    index: 2,
+    description: "Task 2",
+    completed: true,
+  },
+  {
+    index: 3,
+    description: "Task 3",
+    completed: false,
+  }]
+
+  it('Should delete tasks which are completed', () => {
+    expect(deleteCompletedTodoItem(task)).toHaveLength(2)
+  });
+    deleteCompletedTodoItem(task)
+  it('filter should identify tasks which are completed', () => {
+    expect(task).toBe(1)
+  });
 })
